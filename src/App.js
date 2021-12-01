c27038d6037fb3113ff09e916c67cdc7f6eb8072
ResminiLoris <resminiloris@gmail.com>
2021-12-01 15:17:02 +0100

    added city & weather (not dynamic)

--- src/App.js
+++ src/App.js
@@ -15,6 +15,16 @@ function App() {
           <div className="icon">
             <i className="fa fa-sun"></i>
           </div>
+          <h4>weather</h4>
+          <div className="temprature">
+            <h4>25&deg;C</h4>
+          </div>
+          <div className="city">
+            <h4><i className="fa fa-street-view"></i>città | stato</h4>
+          </div>
+          <div className="temprature-range">
+            <h6>Min: 25&deg;C | Max: 28&deg;C | Humidity: 10%</h6>
+          </div>
         </div>
       </div>
     </div>
